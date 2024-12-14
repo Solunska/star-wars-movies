5d3518e1b69b83831002107773b26bb372c4f8e0
solunska <a_solunska@outlook.com>
2024-12-14 19:12:38 +0100

    movie list UI finished

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -8,22 +8,20 @@ export default function Movie({ id, title, director, releaseDate, producers }) {
     const formattedDate = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long', day: 'numeric' }).format(date);
 
     return <div className={classes.container}>
-        <div className={classes.header}>
-            <p className={classes.id}>{movieId}</p>
-            <p className={classes.title}>{title}</p>
-            <p className={classes.date}>{formattedDate}</p>
-            <div className={classes.director}>
-                <p>Directed by</p>
-                <p>{director}</p>
-            </div>
+        <p className={classes.id}>{movieId}</p>
+        <p className={classes.title}>{title}</p>
+        <div className={classes.group}>
+            <p className={classes.label}>Directed by</p>
+            <p className={classes.director}>{director}</p>
         </div>
-        <div className={classes.producerSection}>
-            <p className={classes.producerTitle}>Producers:</p>
+        <div className={classes.group}>
+            <p className={classes.label}>Producers</p>
             <ul className={classes.producerList}>
                 {producers.map((producer) => (
-                    <li key={producer}>{producer}</li>
+                    <li key={producer} className={classes.producer}>{producer}</li>
                 ))}
             </ul>
         </div>
+        <p className={classes.date}>{formattedDate}</p>
     </div>
 }
